80a03e88f0e813afe6bfb643fbf478c0267545b7
Razexay <rando.kangur1@gmail.com>
2019-12-09 15:52:11 +0200

    Search

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php
+
+
+    <?php
     $host = '127.0.0.1';
     $db   = 'Books';
     $user = 'root';
@@ -16,23 +18,45 @@
     } catch (\PDOException $e) {
          throw new \PDOException($e->getMessage(), (int)$e->getCode());
     }
-    $stmt = $pdo->prepare('SELECT * FROM books WHERE release_date = :year');
-    $stmt->execute(['year' => 2000]);
-    while ($row = $stmt->fetch())
-    {
-        echo $row['title'] . "<br>";
-    }
+
+//var_dump($_GET);
+
+$title = $_GET['title'];
+$year = $_GET['year'];
+$stmt = $pdo->prepare('SELECT * FROM books WHERE title LIKE :title OR release_date=:year');
+$stmt->execute(['year' => $year, 'title' => '%' . $title . '%']);
+
 ?>
 
-<form method="POST" action="/index.php">
-    <input type="text">
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>          
+    <h1>Otsing</h1>
+    <form action="index.php" method="get">
+        <input type="text" name="title" placeholder="Pealkiri" value='<?=$title;?>' style="margin: 4px">
+        <br>
+        <input type="text" name="year" placeholder="Aasta" value='<?=$year;?>'style="margin: 4px">'
+        <br>
+        <input type="submit" value="kohuke kohuke, tohuvapohuke" style="margin: 4px">
+    </form>
+    <ul>
 
 <?php
 
-$stmt = $pdo->prepare('SELECT * FROM books WHERE release_date LIKE :year');
-$stmt->execute(['year' => 2000]);
-
 while ( $row = $stmt->fetch() ) {
-    echo $row['title'] . '<br>';
+    echo "<li>" . $row['title'] . "</li>";
 }
+?>
+</ul>
+</body>
+</html>
+
+    
+
+
