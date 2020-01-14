020ac2dcf7074710d0767b4347a0d6d5c3630e46
Razexay <rando.kangur1@gmail.com>
2020-01-14 11:32:46 +0200

    books

--- book.php
+++ book.php
@@ -3,9 +3,9 @@
 require_once 'db_connection.php';
 
 $id = $_GET['id'];
-$stmt = $pdo->prepare('SELECT * FROM books WHERE id = :id');
+$stmt = $pdo->prepare('SELECT * FROM books b LEFT JOIN book_authors ba ON book_id = b.id LEFT JOIN authors a ON author_id = a.id WHERE b.id = :id');
 $stmt->execute(['id' => $id]);
-$book = $stmt->fetch();
+$book = $stmt->fetch()
 
 ?>
 <!DOCTYPE html>
@@ -14,9 +14,24 @@ $book = $stmt->fetch();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Inconsolata&display=swap" rel="stylesheet">
     <title><?= $book['title']; ?></title>
 </head>
 <body>
+    <h1><?= $book['title']; ?></h1>
+    <img src="<?= $book['cover_path']; ?>" alt="" class="center"> 
+    <button id="back" onclick="history.back()"> ⫷ </button> 
+    <div class="flex-container">
+        <div> <?= $book['release_date']; ?></div>
+        <p>KEEL: <?= $book['language']; ?></p>
+        <p>LEHEKÜLJED: <?= $book['pages']; ?></p>
+        <p>HIND: <?= round($book['price'], 2); ?>€</p>
+        <p>SAADAVUS: <?= $book['stock_saldo']; ?></p>
+        <p>OLEK: <?= $book['type']; ?></p>
+        <p>AUTOR: <?= $book['first_name']; ?> <?= $book['last_name']; ?></p>
+    </div>
+        <div class="summary">KIRJELDUS: <?= $book['summary']; ?></div>
     
 </body>
 </html>
--- index.php
+++ index.php
@@ -8,25 +8,27 @@ $stmt = $pdo->prepare('SELECT * FROM books WHERE title LIKE :title OR release_da
 $stmt->execute(['year' => $year, 'title' => '%' . $title . '%']);
 
 ?>
-
+<?php  include('server.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link href="https://fonts.googleapis.com/css?family=Inconsolata&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="index_style.css">
     <title>Document</title>
 </head>
-<body>          
+<body style="background-color:#ffa64d;">     
     <h1>Otsing</h1>
     <form action="index.php" method="get">
-        <input type="text" name="title" placeholder="Pealkiri" value='<?=$title;?>' style="margin: 4px">
+        <input type="text" name="title" placeholder="Pealkiri" style="border-radius: 5px; margin: 4px; border: none; padding: 4px;" value='<?=$title;?>'>
         <br>
-        <input type="text" name="year" placeholder="Aasta" value='<?=$year;?>'style="margin: 4px">'
+        <input type="text" name="year" placeholder="Aasta" style="border-radius: 5px; margin: 4px; border: none; padding: 4px;" value='<?=$year;?>'>
         <br>
-        <input type="submit" value="Code mad" style="margin: 4px">
+        <input type="submit" value="Code mad" style="padding: 5px; font-size: 15px; color: white; background: #4d2600; border: none; border-radius: 5px; margin-left: 5px; margin: 4px;">
     </form>
-    <ul>
+    <ul style="color: #4d2600; font-size: 25px;">
 
 <?php
 
